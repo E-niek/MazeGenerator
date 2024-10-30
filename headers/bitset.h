431261f7111ee0dc0cf04b36df91347c96e9245c
Eniek <121483853+E-niek@users.noreply.github.com>
2024-10-30 21:56:12 +0100

    Write/read bits

--- headers/bitset.h
+++ headers/bitset.h
@@ -1,1 +1,11 @@
-typedef char bitset;
+#ifndef BITSET_H
+#define BITSET_H
+
+#include "utilities.h"
+
+typedef unsigned char bitset[];
+
+bool writeBit(bitset maze, int byte, int bit, int value);
+char readBit(bitset maze, int byte, int bit);
+
+#endif
