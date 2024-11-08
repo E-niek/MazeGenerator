58834d1586910985d532b8ce3f246d71cee13a29
Eniek <121483853+E-niek@users.noreply.github.com>
2024-11-08 17:31:01 +0100

    File import

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # MazeGenerator
 Maze generator, written in C
+
+### Options:
+- -r: amount of rows
+- -c: amount of columns
+- -r: amount of rows
+- -c: amount of columns
+- -s: seed
+- -o: output; file, stdout
+- -n: if the output is a file, specify the name of the file here
+- -f: input a file to generate the maze from
+- -h: show this help
--- src/bitset.c
+++ src/bitset.c
@@ -3,14 +3,16 @@
 
 void writeBit(bitset array, int bitIndex, int value)
 {
-    int byteIndex = bitIndex / 8;
+    // we add 1 because the first byte is used to store the length of the array
+    int byteIndex = bitIndex / 8 + 1;
     int bitOffset = bitIndex % 8;
     array[byteIndex] = value == 0 ? ~((1 << bitOffset) | ~array[byteIndex]) : 1 << bitOffset | array[byteIndex] ;
 }
 
 uint8_t readBit(bitset array, int bitIndex)
 {
-    int byteIndex = bitIndex / 8;
+    // we add 1 because the first byte is used to store the length of the array
+    int byteIndex = bitIndex / 8 + 1;
     int bitOffset = bitIndex % 8;
     return ((1 << bitOffset) & array[byteIndex]) == 1 << bitOffset ? 1 : 0;
 }
