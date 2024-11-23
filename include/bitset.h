486930f2da7e5f989aac129193882bb48cd3734d
Eniek Karels <wgkarels@outlook.com>
2024-11-23 18:32:53 +0100

    Unsafe code fixes and improvements

--- include/bitset.h
+++ include/bitset.h
@@ -5,8 +5,9 @@
 
 typedef uint8_t* bitset;
 
-void writeBit(bitset array, int bitIndex, int byteOffset, int value);
-uint8_t readBit(bitset array, int bitIndex, int byteOffset);
+void writeBit(bitset array, int bitIndex, int value);
+uint8_t readBit(bitset array, int bitIndex);
+uint8_t readBitLog(bitset array, int bitIndex);
 void clearBitset(bitset array, int length);
 
 #endif
